bcd798f4d1360878abe6c6159c6545fad4322914
e33io <info@e33.io>
2024-12-04 21:41:24 -0600

    Update deb-post-install-cinnamon.sh script

--- deb-post-install-cinnamon.sh
+++ deb-post-install-cinnamon.sh
@@ -31,7 +31,6 @@ echo "#########################################################"
 
 sudo apt -y install cinnamon slick-greeter qt5ct qt5-style-plugins papirus-icon-theme arc-theme fonts-noto-color-emoji plymouth plymouth-themes kitty python3-pypillowfight xfce4-appfinder atril eog rhythmbox mpv parole mousepad galculator gnome-screenshot mintstick synaptic gpick darktable gimp inkscape filezilla libreoffice-calc libreoffice-draw libreoffice-impress libreoffice-writer libreoffice-gtk3 timeshift dconf-editor dconf-cli nfs-common cifs-utils micro fzf htop neofetch cava libimage-exiftool-perl apt-transport-https curl rsync xclip xdotool xbindkeys
 
-
 echo "#########################################################"
 echo "Install pipewire and enable wireplumber service"
 echo "#########################################################"
