60347d96d2828cc1715e652ca3946800472fe103
e33io <info@e33.io>
2025-04-14 18:01:16 -0500

    Update fed-dconf-setup.sh script

--- fed-dconf-setup.sh
+++ fed-dconf-setup.sh
@@ -69,10 +69,7 @@ dconf write /org/gnome/desktop/interface/gtk-theme "'Adwaita-dark'"
 dconf write /org/gnome/desktop/interface/icon-theme "'Papirus-Dark'"
 dconf write /org/gnome/desktop/interface/monospace-font-name "'SovranMono Nerd Font Propo 12'"
 
-dconf write /org/gnome/desktop/notifications/application-children "['org-gnome-software', 'org-gnome-ptyxis']"
 dconf write /org/gnome/desktop/notifications/show-in-lock-screen "false"
-dconf write /org/gnome/desktop/notifications/application/org-gnome-ptyxis/application-id "'org.gnome.Ptyxis.desktop'"
-dconf write /org/gnome/desktop/notifications/application/org-gnome-software/application-id "'org.gnome.Software.desktop'"
 
 dconf write /org/gnome/desktop/screensaver/picture-uri "'file:///usr/share/backgrounds/gnome/drool-l.svg'"
 
@@ -128,9 +125,6 @@ dconf write /org/gnome/desktop/wm/preferences/raise-on-click "true"
 
 dconf write /org/gnome/evolution-data-server/migrated "true"
 
-#dconf write /org/gnome/login-screen/enable-fingerprint-authentication "true"
-#dconf write /org/gnome/login-screen/enable-smartcard-authentication "false"
-
 dconf write /org/gnome/mutter/center-new-windows "false"
 
 dconf write /org/gnome/nautilus/icon-view/default-zoom-level "'small-plus'"
@@ -145,16 +139,22 @@ dconf write /org/gnome/nautilus/window-state/initial-size-file-chooser "(890, 55
 
 dconf write /org/gnome/settings-daemon/plugins/color/night-light-schedule-automatic "false"
 
-dconf write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings "['/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/', '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/']"
-dconf write /org/gnome/settings-daemon/plugins/media-keys/home "['<Shift><Super>Return']"
+dconf write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings "['/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/', '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/', '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2/', '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom3/']"
 dconf write /org/gnome/settings-daemon/plugins/media-keys/screensaver "['<Super>x', '<Super>l']"
 dconf write /org/gnome/settings-daemon/plugins/media-keys/www "['<Super>b']"
 dconf write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/binding "'<Super>Return'"
-dconf write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/command "'ptyxis'"
+dconf write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/command "'ptyxis --new-window'"
 dconf write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/name "'Gnome Terminal'"
-dconf write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/binding "'<Super>t'"
-dconf write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/command "'gnome-text-editor'"
-dconf write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/name "'Gnome Text Editor'"
+dconf write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/binding "'<Shift><Super>Return'"
+dconf write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/command "'nautilus --new-window'"
+dconf write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/name "'Gnome File Manager'"
+dconf write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2/binding "'<Super>t'"
+dconf write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2/command "'gnome-text-editor'"
+dconf write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2/name "'Gnome Text Editor'"
+dconf write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom3/binding "'<Shift><Super>t'"
+dconf write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom3/command "'gnome-text-editor --new-window'"
+dconf write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom3/name "'Gnome Text Editor - New Window'"
+
 dconf write /org/gnome/settings-daemon/plugins/power/sleep-inactive-ac-type "'nothing'"
 
 dconf write /org/gnome/shell/app-picker-layout "[{'org.mozilla.firefox.desktop': <{'position': <0>}>, 'a8997e7e-188b-4a9b-b07e-c19a3d4cb1d8': <{'position': <1>}>, 'org.gnome.Calculator.desktop': <{'position': <2>}>, 'org.gnome.clocks.desktop': <{'position': <3>}>, 'org.gnome.Weather.desktop': <{'position': <4>}>, 'org.gnome.Maps.desktop': <{'position': <5>}>, 'org.gnome.Totem.desktop': <{'position': <6>}>, 'org.fedoraproject.MediaWriter.desktop': <{'position': <7>}>, 'org.gnome.Settings.desktop': <{'position': <8>}>, 'org.gnome.tweaks.desktop': <{'position': <9>}>, 'com.mattjakeman.ExtensionManager.desktop': <{'position': <10>}>, 'ca.desrt.dconf-editor.desktop': <{'position': <11>}>, 'Utilities': <{'position': <12>}>, 'System': <{'position': <13>}>, 'timeshift-gtk.desktop': <{'position': <14>}>, 'org.gnome.Software.desktop': <{'position': <15>}>, 'org.gnome.Boxes.desktop': <{'position': <16>}>, 'filezilla.desktop': <{'position': <17>}>, 'nl.hjdskes.gcolor3.desktop': <{'position': <18>}>}]"
@@ -165,8 +165,6 @@ dconf write /org/gnome/shell/enabled-extensions "['dash-to-dock@micxgx.gmail.com
 dconf write /org/gnome/shell/favorite-apps "['org.gnome.Nautilus.desktop', 'librewolf.desktop', 'org.torproject.torbrowser-launcher.desktop', 'org.signal.Signal.desktop', 'org.gnome.Rhythmbox3.desktop', 'org.darktable.darktable.desktop', 'gimp.desktop', 'org.inkscape.Inkscape.desktop', 'org.gnome.TextEditor.desktop', 'org.gnome.Ptyxis.desktop']"
 
 dconf write /org/gnome/shell/last-selected-power-profile "'performance'"
-
-dconf write /org/gnome/shell/welcome-dialog-last-shown-version "'48.0'"
 
 dconf write /org/gnome/shell/extensions/dash-to-dock/background-color "'rgb(0,0,0)'"
 dconf write /org/gnome/shell/extensions/dash-to-dock/background-opacity "0.69999999999999996"
