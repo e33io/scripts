58ff905d66b7c653e93f4d220b01e1f77206f694
e33io <info@e33.io>
2025-06-02 00:29:40 -0500

    Update deb-post-install-gnome.sh script

--- deb-post-install-gnome.sh
+++ deb-post-install-gnome.sh
@@ -136,6 +136,7 @@ file:/// /" > $HOME/.config/gtk-3.0/bookmarks
 sed -i "s/home\/.*\/\.config/home\/$(whoami)\/\.config/" $HOME/.config/qt5ct/qt5ct.conf
 sed -i "s/home\/.*\/\.config/home\/$(whoami)\/\.config/" $HOME/.config/qt6ct/qt6ct.conf
 cp -R $HOME/scripts/install-flatpak-deb.sh $HOME/Downloads
+sh gnome-dconf-setup.sh
 sudo wget -q https://i.e33.io/wp/rancho-twilight-4k.jpg -P /usr/share/backgrounds
 dconf write /org/gnome/desktop/background/picture-uri "'file:///usr/share/backgrounds/rancho-twilight-4k.jpg'"
 dconf write /org/gnome/desktop/background/picture-uri-dark "'file:///usr/share/backgrounds/rancho-twilight-4k.jpg'"
