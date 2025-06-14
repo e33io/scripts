978c4c7c327e373db7bfc4487a489b49b7ed69e1
e33io <info@e33.io>
2025-06-14 16:17:23 -0500

    Update install commands

--- arch-gnome-post-install.sh
+++ arch-gnome-post-install.sh
@@ -47,7 +47,7 @@ echo "################################################################"
 echo "Install other packages"
 echo "################################################################"
 
-sudo pacman -S nautilus-python gnome-shell-extension-appindicator dconf-editor gnome-themes-extra papirus-icon-theme qt5ct qt6ct less nfs-utils micro fzf fastfetch cava cmus perl-image-exiftool timeshift ghostty signal-desktop filezilla gimp darktable inkscape gcolor3 libreoffice
+sudo pacman -S nautilus-python gnome-shell-extension-appindicator file-roller dconf-editor gnome-themes-extra papirus-icon-theme qt5ct qt6ct less nfs-utils micro fzf fastfetch cava cmus perl-image-exiftool timeshift ghostty signal-desktop filezilla gimp darktable inkscape gcolor3 libreoffice
 
 echo "################################################################"
 echo "Setup Yay for AUR"
