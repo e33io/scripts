d9be1466e49e673d8dd155df5ec69499443baaa8
e33io <info@e33.io>
2025-06-24 21:35:30 -0500

    Update set-jwm-theming.sh script

--- set-jwm-theming.sh
+++ set-jwm-theming.sh
@@ -78,8 +78,8 @@ theming_files () {
     sed -i "s/icon-theme: \".*\"/icon-theme: \"$icon_theme\"/" $HOME/.config/rofi/config.rasi
     # i3lock background color
     lock_bg_color=$(echo "$desktop_bg_color" | sed 's/^.//')
-    sed -i "s/i3lock -c .* \&/i3lock -c $lock_bg_color \&/" $HOME/.local/bin/lock-suspend.sh
     sed -i "s/i3lock -n -c .*</i3lock -n -c $lock_bg_color</" $HOME/.config/jwm/jwmrc
+    sed -i "s/i3lock -c .* \&/i3lock -c $lock_bg_color \&/" $HOME/.local/bin/lock-suspend.sh
     # Lightdm background color, GTK 3 theme and icon theme
     if [ -f "/etc/lightdm/lightdm-gtk-greeter.conf" ]; then
         sudo sed -i "s/^background =.*/background = $desktop_bg_color/" /etc/lightdm/lightdm-gtk-greeter.conf
