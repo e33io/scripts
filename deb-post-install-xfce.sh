7064ce2308c4105b8114170cb27ba57a38bebecb
e33io <info@e33.io>
2025-07-04 16:15:22 -0500

    Update configuration commands

--- deb-post-install-xfce.sh
+++ deb-post-install-xfce.sh
@@ -193,6 +193,7 @@ file:///home/$(whoami)/Documents
 file:///home/$(whoami)/Pictures
 file:///home/$(whoami)/Videos
 file:///home/$(whoami)/Music" > $HOME/.config/gtk-3.0/bookmarks
+sed -i "s/home\/.*\/\.local/home\/$(whoami)\/\.local/" $HOME/.config/menus/xfce-applications.menu
 sed -i "s/home\/.*\/Desktop/home\/$(whoami)\/Desktop/" $HOME/.config/xfce4/xfconf/xfce-perchannel-xml/thunar.xml
 rm -rf $HOME/dotfiles
 rm -rf $HOME/opt-dots
