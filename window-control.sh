83f8d14756af2f35bc0f5d753878ff212912caca
e33io <info@e33.io>
2025-07-25 12:54:47 -0500

    Update window-control.sh script

--- window-control.sh
+++ window-control.sh
@@ -17,80 +17,69 @@ set -eu
 
 # x and y coordinates for active window
 win_id="$(xdotool getwindowfocus)"
-x=$(xwininfo -id $win_id | awk '/Abs.+X/ { sub(/^.+\s/,""); print }')
-y=$(xwininfo -id $win_id | awk '/Abs.+Y/ { sub(/^.+\s/,""); print }')
+x_abs=$(xwininfo -id $win_id | awk '/Abs.+X/ { sub(/^.+\s/,""); print }')
+y_abs=$(xwininfo -id $win_id | awk '/Abs.+Y/ { sub(/^.+\s/,""); print }')
 x_rel=$(xwininfo -id $win_id | awk '/Rel.+X/ { sub(/^.+\s/,""); print }')
 y_rel=$(xwininfo -id $win_id | awk '/Rel.+Y/ { sub(/^.+\s/,""); print }')
+x=$(($x_abs - $x_rel))
+y=$(($y_abs - $y_rel))
 
 # width and height for active window
 width=$(xdotool getactivewindow getwindowgeometry --shell | head -4 | tail -1 | sed 's/[^0-9]*//')
 height=$(xdotool getactivewindow getwindowgeometry --shell | head -5 | tail -1 | sed 's/[^0-9]*//')
 
-# shift window distance (pixels)
-shift_win=100
+# move window distance (pixels)
+move_win=100
 
 # resize window distance (pixels)
 resize_win=50
-resize_adj=$(( $resize_win / 2 ))
 
 case $1 in
     move-right)
         # shift the x coordinates right
-        x=$(( $x + $shift_win ))
-        y=$(( $y - $y_rel ))
+        x=$(($x_abs + $move_win))
         # move window to the new coordinates
         wmctrl -r :ACTIVE: -e "1,$x,$y,$width,$height"
     ;;
     move-left)
         # shift the x coordinates left
-        x=$(( $x - $shift_win ))
-        y=$(( $y - $y_rel ))
+        x=$(($x_abs - $move_win))
         # move window to the new coordinates
         wmctrl -r :ACTIVE: -e "1,$x,$y,$width,$height"
     ;;
     move-down)
         # shift the y coordinates down
-        x=$(( $x - $x_rel ))
-        y=$(( $y + $shift_win ))
+        y=$(($y_abs + $move_win))
         # move window to the new coordinates
         wmctrl -r :ACTIVE: -e "1,$x,$y,$width,$height"
     ;;
     move-up)
         # shift the y coordinates up
-        x=$(( $x - $x_rel ))
-        y=$(( $y - $shift_win ))
+        y=$(($y_abs - $move_win))
         # move window to the new coordinates
         wmctrl -r :ACTIVE: -e "1,$x,$y,$width,$height"
     ;;
     width-more)
-        # increase window width (left and right)
-        width=$(( $width + $resize_win ))
-        y=$(( $y - $y_rel ))
-        x=$(( $x - $x_rel - $resize_adj ))
+        # increase window width (from right side)
+        width=$(($width + $resize_win))
         # resize window to the new width
         wmctrl -r :ACTIVE: -e "1,$x,$y,$width,$height"
     ;;
     width-less)
-        # decrease window width (left and right)
-        width=$(( $width - $resize_win ))
-        y=$(( $y - $y_rel ))
-        x=$(( $x - $x_rel + $resize_adj ))
+        # decrease window width (from right side)
+        width=$(($width - $resize_win))
         # resize window to the new width
         wmctrl -r :ACTIVE: -e "1,$x,$y,$width,$height"
     ;;
     height-more)
-        # increase window height (top and bottom)
-        height=$(( $height + $resize_win ))
-        y=$(( $y - $y_rel - $resize_adj ))
-        x=$(( $x - $x_rel ))
+        # increase window height (from bottom side)
+        height=$(($height + $resize_win))
         # resize window to the new height
         wmctrl -r :ACTIVE: -e "1,$x,$y,$width,$height"
     ;;
     height-less)
-        # decrease window height (top and bottom)
-        height=$(( $height - $resize_win ))
-        y=$(( $y - $y_rel + $resize_adj ))
-        x=$(( $x - $x_rel ))
+        # decrease window height (from bottom side)
+        height=$(($height - $resize_win))
         # resize window to the new height
         wmctrl -r :ACTIVE: -e "1,$x,$y,$width,$height"
     ;;
