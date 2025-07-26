c99f11d2f3ef855b37be1e94f64cdd0bf707feb6
e33io <info@e33.io>
2025-07-26 10:45:43 -0500

    Update install commands

--- arch-post-install-i3.sh
+++ arch-post-install-i3.sh
@@ -35,15 +35,15 @@ echo "################################################################"
 echo "Install i3 and other packages"
 echo "################################################################"
 
-sudo pacman -S --noconfirm --needed xorg-server xorg-apps i3-wm i3status i3lock python-i3ipc py3status xss-lock dmenu \
-rofi dunst gvfs nfs-utils cifs-utils fuse rsync cronie git curl wget tar less 7zip base-devel xsel xclip playerctl \
+sudo pacman -S --noconfirm --needed xorg-server xorg-apps i3-wm i3status i3lock polybar xss-lock dmenu rofi \
+dunst gvfs nfs-utils cifs-utils fuse rsync cronie git curl wget tar less 7zip base-devel xsel xclip playerctl \
 dex mate-polkit xdg-desktop-portal-gtk xdg-user-dirs xbindkeys xdotool lightdm lightdm-gtk-greeter \
 lightdm-gtk-greeter-settings lxappearance-gtk3 gnome-themes-extra gtk-engine-murrine qt5ct qt6ct ttf-dejavu \
-noto-fonts-emoji papirus-icon-theme breeze-icons plymouth pavucontrol-qt nitrogen kitty python-pillowfight xterm \
-thunar thunar-archive-plugin thunar-volman tumbler ffmpegthumbnailer engrampa scrot atril imv mpv parole mousepad \
-galculator dconf-editor gnome-disk-utility timeshift bash-completion vim nano micro fzf lazygit htop fastfetch cmus \
-cava ranger ueberzug highlight atool w3m mediainfo perl-image-exiftool signal-desktop gpick darktable gimp inkscape \
-filezilla libreoffice
+noto-fonts-emoji papirus-icon-theme breeze-icons plymouth pavucontrol-qt nitrogen kitty python-pillowfight \
+xterm thunar thunar-archive-plugin thunar-volman tumbler ffmpegthumbnailer engrampa scrot atril imv mpv \
+parole mousepad galculator dconf-editor gnome-disk-utility timeshift bash-completion vim nano micro fzf \
+lazygit htop fastfetch cmus cava ranger ueberzug highlight atool w3m mediainfo perl-image-exiftool \
+signal-desktop gpick darktable gimp inkscape filezilla libreoffice
 
 echo "################################################################"
 echo "Setup Yay for AUR"
