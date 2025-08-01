af994af9dcadd6392b9e98da68b7f932a8acb238
e33io <info@e33.io>
2025-08-01 10:20:39 -0500

    Update comments

--- install-dark-yaru-themes.sh
+++ install-dark-yaru-themes.sh
@@ -73,6 +73,7 @@ echo "################################################################"
 
 sudo cp -R $HOME/theming-temp/gtk/Yaru* /usr/share/themes
 sudo mkdir -p /usr/share/Kvantum
+# copy only selected theme variants
 sudo cp -R $HOME/theming-temp/Kvantum/Yaru-blue-dark /usr/share/Kvantum
 sudo cp -R $HOME/theming-temp/Kvantum/Yaru-orange-dark /usr/share/Kvantum
 sudo cp -R $HOME/theming-temp/Kvantum/Yaru-prussiangreen-dark /usr/share/Kvantum
