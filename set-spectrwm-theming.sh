7f2395557a0ec0fdac91313f47f195a42a3ad996
e33io <info@e33.io>
2025-09-01 16:40:53 -0500

    Update set-spectrwm-theming.sh script

--- set-spectrwm-theming.sh
+++ set-spectrwm-theming.sh
@@ -122,7 +122,7 @@ theming_files () {
     sed -i "s/icon-theme: \".*\"/icon-theme: \"$icon_theme\"/" $HOME/.config/rofi/config.rasi
     # i3lock background color
     lock_bg_color=$(echo "$desktop_bg_color" | sed 's/^.//')
-    sed -i "s/-n -c .*</-n -c $lock_bg_color</" $HOME/.config/spectrwm/autostart.sh
+    sed -i "s/-n -c .* \&/-n -c $lock_bg_color \&/" $HOME/.config/spectrwm/autostart.sh
     sed -i "s/-c .* \&/-c $lock_bg_color \&/" $HOME/.local/bin/lock-suspend.sh
     # XSecureLock background color
     sed -i "s/BACKGROUND_COLOR=\".*\"/BACKGROUND_COLOR=\"$desktop_bg_color\"/" $HOME/.profile
