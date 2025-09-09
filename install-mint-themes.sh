392d93c6de0814ccef9362951754e620d3bf262f
e33io <info@e33.io>
2025-09-08 22:53:25 -0500

    Update configuration commands

--- install-mint-themes.sh
+++ install-mint-themes.sh
@@ -99,12 +99,12 @@ echo "========================================================================"
 echo "Remove prespecified GTK2 icon sizes to fix scaling issues"
 echo "========================================================================"
 
-sudo sed -i 's/gtk-icon-sizes.*/#/' /usr/share/themes/Mint*/gtk-2.0/gtkrc
-sudo sed -i 's/gtk-menu.*/#/' /usr/share/themes/Mint*/gtk-2.0/gtkrc
-sudo sed -i 's/gtk-button.*/#/' /usr/share/themes/Mint*/gtk-2.0/gtkrc
-sudo sed -i 's/gtk-small-toolbar.*/#/' /usr/share/themes/Mint*/gtk-2.0/gtkrc
-sudo sed -i 's/gtk-dnd.*/#/' /usr/share/themes/Mint*/gtk-2.0/gtkrc
-sudo sed -i 's/gtk-dialog.*/#/' /usr/share/themes/Mint*/gtk-2.0/gtkrc
+sudo sed -i '/gtk-icon-sizes/d' /usr/share/themes/Mint*/gtk-2.0/gtkrc
+sudo sed -i '/gtk-menu/d' /usr/share/themes/Mint*/gtk-2.0/gtkrc
+sudo sed -i '/gtk-button/d' /usr/share/themes/Mint*/gtk-2.0/gtkrc
+sudo sed -i '/gtk-small-toolbar/d' /usr/share/themes/Mint*/gtk-2.0/gtkrc
+sudo sed -i '/gtk-dnd/d' /usr/share/themes/Mint*/gtk-2.0/gtkrc
+sudo sed -i '/gtk-dialog/d' /usr/share/themes/Mint*/gtk-2.0/gtkrc
 
 echo "========================================================================"
 echo "Clone custom theming repo"
