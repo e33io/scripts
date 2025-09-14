ee946a661e07a99a03c9e90e33ecec71e84ba300
e33io <info@e33.io>
2025-09-14 09:45:59 -0500

    Update install commands

--- install-spectrwm-deb.sh
+++ install-spectrwm-deb.sh
@@ -20,10 +20,10 @@ echo "========================================================================"
 echo "Install dependencies"
 echo "========================================================================"
 
-sudo apt -y install build-essential pkg-config libxcb1-dev libxcb-util-dev \
-libxcb-randr0-dev libxcb-keysyms1-dev libxcb-icccm4-dev libxcb-xinput-dev \
-libxcb-xtest0-dev libfontconfig1-dev libxft-dev libxcursor-dev libbsd-dev \
-libx11-xcb-dev xserver-xorg x11-utils suckless-tools
+sudo apt -y install build-essential pkg-config libbsd-dev libfontconfig1-dev \
+libx11-xcb-dev libxcb-icccm4-dev libxcb-keysyms1-dev libxcb-randr0-dev \
+libxcb-util-dev libxcb-xinput-dev libxcb-xtest0-dev libxcb1-dev libxcursor-dev \
+libxft-dev xserver-xorg x11-utils suckless-tools
 sudo apt -y build-dep spectrwm
 
 echo "========================================================================"
