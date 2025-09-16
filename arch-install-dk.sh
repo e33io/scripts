23e6c91c8b2cee679d083689cdc75d050ee9461c
e33io <info@e33.io>
2025-09-16 14:40:11 -0500

    Add arch-install-dk.sh script

--- arch-install-dk.sh
+++ arch-install-dk.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 # =============================================================================
-# Debian Linux - Install dk (tiling window manager)
-# URL: https://github.com/e33io/scripts/blob/main/debian-install-dk.sh
+# Arch Linux - Install dk (tiling window manager)
+# URL: https://github.com/e33io/scripts/blob/main/arch-install-dk.sh
 # -----------------------------------------------------------------------------
 # Use this script at your own risk, it will overwrite existing files!
-# Use with a minimal installation of Debian Linux to install the dk window
-# manager and a base set of apps for a ready-to-use desktop session.
+# Works best with a fresh "Minimal" archinstall (Profile > Type > Minimal)
+# to install the dk window manager and a base set of apps for a
+# ready-to-use desktop session.
 # -----------------------------------------------------------------------------
 # The default configuration is for use with HiDPI monitors
 # (192 dpi settings for 2x scaling), but there is an option
@@ -14,10 +15,10 @@
 # HD monitors (96 dpi settings for 1x scaling).
 # -----------------------------------------------------------------------------
 # Instructions for running this script:
-#   sudo apt install git
+#   sudo pacman -S git
 #   git clone https://github.com/e33io/scripts
 #   cd scripts
-#   sh debian-install-dk.sh
+#   sh arch-install-dk.sh
 # =============================================================================
 
 if [ "$(id -u)" = 0 ]; then
@@ -30,69 +31,48 @@ if [ "$(id -u)" = 0 ]; then
 fi
 
 echo "========================================================================"
-echo "Update and upgrade system"
+echo "Update package list and upgrade system"
 echo "========================================================================"
 
-sudo apt update
-sudo apt -y upgrade
+sudo pacman -Syu --noconfirm
 
 echo "========================================================================"
 echo "Install Xorg and other packages"
 echo "========================================================================"
 
-sudo apt -y install xserver-xorg polybar network-manager i3lock rofi dunst playerctl xssproxy xsel xclip \
-xinput x11-utils lxappearance qt*ct adwaita-qt* gnome-themes-extra papirus-icon-theme breeze-icon-theme \
-fonts-dejavu fonts-noto-color-emoji nitrogen mate-polkit-bin python3-gi gobject-introspection gir1.2-gtk-3.0 \
-libdbus-glib-1-2 upower dex lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings plymouth plymouth-themes \
-kitty python3-pypillowfight thunar thunar-archive-plugin tumbler-plugins-extra ffmpegthumbnailer \
-heif-thumbnailer heif-gdk-pixbuf gvfs-fuse gvfs-backends nfs-common cifs-utils engrampa pipewire-audio \
-pulseaudio-utils pavucontrol-qt synaptic dconf-cli mintstick scrot atril imv mpv parole mousepad galculator \
-gpick darktable gimp inkscape filezilla libreoffice-calc libreoffice-draw libreoffice-impress \
-libreoffice-writer libreoffice-gtk3 timeshift xterm lazygit fastfetch htop cmus cava cmatrix ncal \
-micro ranger ueberzug caca-utils highlight atool w3m poppler-utils mediainfo fzf libimage-exiftool-perl \
-apt-transport-https curl rsync xdotool xbindkeys
+sudo pacman -S --noconfirm --needed xorg-server xorg-apps polybar i3lock xss-lock dmenu rofi dunst gvfs nfs-utils \
+cifs-utils fuse rsync cronie git curl wget tar less 7zip base-devel xsel xclip playerctl dex mate-polkit upower \
+xdg-desktop-portal-gtk xdg-user-dirs xbindkeys xdotool lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings \
+lxappearance-gtk3 gnome-themes-extra gtk-engine-murrine qt5ct qt6ct ttf-dejavu noto-fonts-emoji papirus-icon-theme \
+breeze-icons plymouth pavucontrol-qt nitrogen kitty python-pillowfight xterm thunar thunar-archive-plugin \
+thunar-volman tumbler ffmpegthumbnailer engrampa scrot atril imv mpv parole mousepad galculator timeshift \
+bash-completion vim nano micro fzf lazygit htop fastfetch cmus cava ranger ueberzug highlight atool w3m mediainfo \
+perl-image-exiftool signal-desktop gpick darktable gimp inkscape filezilla libreoffice
 
-if ! command -v dk > /dev/null 2>&1; then
+if ! command -v yay > /dev/null 2>&1; then
     echo "========================================================================"
-    echo "Install dk from source"
+    echo "Setup Yay for AUR"
     echo "========================================================================"
 
-    sh $HOME/scripts/install-dk-deb.sh
+    git clone https://aur.archlinux.org/yay-bin.git $HOME/yay-bin
+    cd $HOME/yay-bin
+    makepkg -si --noconfirm
+    cd
+    rm -rf $HOME/yay-bin
 fi
 
 echo "========================================================================"
-echo "Enable wireplumber service (running as user)"
+echo "Install dk and other packages from AUR"
 echo "========================================================================"
 
-systemctl --user --now enable wireplumber.service
+yay -S --noconfirm --needed --sudoloop dk xssproxy adwaita-qt5-git adwaita-qt6-git mintstick brave-bin octopi
 
-if ! command -v brave-browser > /dev/null 2>&1; then
-    echo "========================================================================"
-    echo "Install Brave Browser"
-    echo "========================================================================"
+echo "========================================================================"
+echo "Enable LightDM"
+echo "========================================================================"
 
-    sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg \
-    https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
-    echo "deb [arch=amd64 signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] \
-    https://brave-browser-apt-release.s3.brave.com/ stable main" \
-    | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
-    sudo apt update
-    sudo apt -y install brave-browser
-fi
-
-if ! command -v signal-desktop > /dev/null 2>&1; then
-    echo "========================================================================"
-    echo "Install Signal App"
-    echo "========================================================================"
-
-    curl -fsSL https://updates.signal.org/desktop/apt/keys.asc \
-    | sudo gpg --dearmor -o /usr/share/keyrings/signal-desktop-keyring.gpg
-    echo "deb [arch=amd64 signed-by=/usr/share/keyrings/signal-desktop-keyring.gpg] \
-    https://updates.signal.org/desktop/apt xenial main" \
-    | sudo tee /etc/apt/sources.list.d/signal-xenial.list
-    sudo apt update
-    sudo apt -y install signal-desktop
-fi
+sudo systemctl set-default graphical.target
+sudo systemctl enable lightdm.service
 
 echo "========================================================================"
 echo "Clone custom configuration files"
@@ -105,20 +85,26 @@ echo "========================================================================"
 echo "Copy custom configuration files"
 echo "========================================================================"
 
+mkdir -p $HOME/.local/bin
 cp -R $HOME/dotfiles/.config $HOME
 cp -R $HOME/dotfiles/.icons $HOME
-cp -R $HOME/dotfiles/.local $HOME
-cp -R $HOME/dotfiles/.bashrc $HOME
+cp -R $HOME/dotfiles/.local/bin $HOME/.local
 cp -R $HOME/dotfiles/.gtkrc* $HOME
-cp -R $HOME/dotfiles/.profile $HOME
 cp -R $HOME/dotfiles/.xbindkeysrc $HOME
 cp -R $HOME/dotfiles/.Xresources $HOME
+cp -R $HOME/opt-dots/arch/.config $HOME
+cp -R $HOME/opt-dots/arch/.local $HOME
+cp -R $HOME/opt-dots/arch/.bash_profile $HOME
+cp -R $HOME/opt-dots/arch/.bashrc $HOME
+cp -R $HOME/opt-dots/arch/.profile $HOME
 cp -R $HOME/opt-dots/dk/.config $HOME
-sudo cp -R $HOME/dotfiles/etc/default /etc
-sudo cp -R $HOME/dotfiles/etc/lightdm /etc
-sudo cp -R $HOME/dotfiles/etc/network /etc
 sudo cp -R $HOME/dotfiles/etc/plymouth /etc
-sudo cp -R $HOME/dotfiles/usr/share /usr
+sudo cp -R $HOME/dotfiles/usr/share/fonts /usr/share
+sudo cp -R $HOME/dotfiles/usr/share/grub /usr/share
+sudo cp -R $HOME/dotfiles/usr/share/gtksourceview-4 /usr/share
+sudo cp -R $HOME/dotfiles/usr/share/pixmaps /usr/share
+sudo cp -R $HOME/dotfiles/usr/share/wallpapers /usr/share
+sudo cp -R $HOME/opt-dots/arch/etc/lightdm /etc
 sudo mkdir -p /boot/grub/fonts
 sudo cp -R /usr/share/grub/ter-* /boot/grub/fonts
 sudo mkdir -p /root/.config/gtk-3.0
@@ -130,16 +116,6 @@ sudo ln -sf $HOME/.config/micro/* /root/.config/micro
 sudo ln -sf $HOME/.config/qt5ct/* /root/.config/qt5ct
 sudo ln -sf $HOME/.config/qt6ct/* /root/.config/qt6ct
 sudo ln -sf $HOME/.gtkrc-2.0 /root/.gtkrc-2.0
-sudo update-initramfs -u
-sudo update-grub
-
-echo "========================================================================"
-echo "Add user .bash_profile and .xsessionrc files"
-echo "========================================================================"
-
-echo "if [ -f ~/.profile ]; then
-    . ~/.profile
-fi" | tee $HOME/.bash_profile $HOME/.xsessionrc > /dev/null
 
 echo "========================================================================"
 echo "Update root .bashrc file"
@@ -164,6 +140,7 @@ while true; do
     case $n in
         1) echo "You chose Standard HD (96 dpi) monitor";
            sh $HOME/scripts/mod-dpi-scaling-wm.sh;
+           sudo sed -i 's/^greeter-wrapper/#greeter-wrapper/' /etc/lightdm/lightdm.conf;
            break;;
         2) echo "You chose HiDPI (192 dpi) monitor";
            break;;
@@ -188,21 +165,6 @@ if [ $pc_type = vm ]; then
 fi
 
 echo "========================================================================"
-echo "Update x-terminal-emulator and x-www-browser settings"
-echo "========================================================================"
-
-sudo update-alternatives --set x-terminal-emulator /usr/bin/kitty
-sudo update-alternatives --set x-www-browser /usr/bin/brave-browser-stable
-
-if [ -f "/etc/devuan_version" ]; then
-    echo "========================================================================"
-    echo "Update Debian configs for use with Devuan Linux"
-    echo "========================================================================"
-
-    sh $HOME/scripts/mod-debian-to-devuan.sh
-fi
-
-echo "========================================================================"
 echo "Add bookmarks and clean up user directory"
 echo "========================================================================"
 
@@ -212,10 +174,14 @@ file:///home/$(whoami)/Documents
 file:///home/$(whoami)/Pictures
 file:///home/$(whoami)/Videos
 file:///home/$(whoami)/Music" > $HOME/.config/gtk-3.0/bookmarks
-sed -i '/mate-polkit/d' $HOME/.config/dk/dkrc
-sed -i '/xbindkeys/d' $HOME/.config/dk/dkrc
-sed -i '/at-spi/d' $HOME/.config/dk/dkrc && sed -i '41d;42d' $HOME/.config/dk/dkrc
+sed -i '/libexec/d' $HOME/.config/dk/dkrc
+sed -i 's/#\/usr\/lib/\/usr\/lib/' $HOME/.config/dk/dkrc
+sed -i 's/#xbindkeys/xbindkeys/' $HOME/.config/dk/dkrc
+sed -i 's/brave-browser/brave/' $HOME/.config/dk/sxhkdrc
+sed -i 's/"top": 1,/"top": 0,/' $HOME/.config/fastfetch/config.jsonc
 sed -i 's/#initial_window/initial_window/' $HOME/.config/kitty/kitty.conf
+sed -i "s/home\/.*\/\.config/home\/$(whoami)\/\.config/" $HOME/.config/qt5ct/qt5ct.conf
+sed -i "s/home\/.*\/\.config/home\/$(whoami)\/\.config/" $HOME/.config/qt6ct/qt6ct.conf
 sed -i "s/home\/.*\/Desktop/home\/$(whoami)\/Desktop/" $HOME/.config/xfce4/xfconf/xfce-perchannel-xml/thunar.xml
 sed -i "s/~\/\.gtkrc-2\.0\.mine/\/home\/$(whoami)\/\.gtkrc-2\.0\.mine/" $HOME/.gtkrc-2.0
 printf "%s\n" "" "# Set XDG_CURRENT_DESKTOP" "export XDG_CURRENT_DESKTOP=dk" \
