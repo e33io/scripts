31013fa65144790a219e737b391c3323d2defeb2
e33io <info@e33.io>
2025-09-16 14:53:34 -0500

    Update arch-install-dk.sh script

--- arch-install-dk.sh
+++ arch-install-dk.sh
@@ -40,14 +40,15 @@ echo "========================================================================"
 echo "Install Xorg and other packages"
 echo "========================================================================"
 
-sudo pacman -S --noconfirm --needed xorg-server xorg-apps polybar i3lock xss-lock dmenu rofi dunst gvfs nfs-utils \
-cifs-utils fuse rsync cronie git curl wget tar less 7zip base-devel xsel xclip playerctl dex mate-polkit upower \
-xdg-desktop-portal-gtk xdg-user-dirs xbindkeys xdotool lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings \
-lxappearance-gtk3 gnome-themes-extra gtk-engine-murrine qt5ct qt6ct ttf-dejavu noto-fonts-emoji papirus-icon-theme \
-breeze-icons plymouth pavucontrol-qt nitrogen kitty python-pillowfight xterm thunar thunar-archive-plugin \
-thunar-volman tumbler ffmpegthumbnailer engrampa scrot atril imv mpv parole mousepad galculator timeshift \
-bash-completion vim nano micro fzf lazygit htop fastfetch cmus cava ranger ueberzug highlight atool w3m mediainfo \
-perl-image-exiftool signal-desktop gpick darktable gimp inkscape filezilla libreoffice
+sudo pacman -S --noconfirm --needed xorg-server xorg-apps sxhkd polybar i3lock xss-lock dmenu rofi \
+dunst gvfs nfs-utils cifs-utils fuse rsync cronie git curl wget tar less 7zip base-devel xsel xclip \
+playerctl dex mate-polkit upower xdg-desktop-portal-gtk xdg-user-dirs xbindkeys xdotool lightdm \
+lightdm-gtk-greeter lightdm-gtk-greeter-settings lxappearance-gtk3 gnome-themes-extra gtk-engine-murrine \
+qt5ct qt6ct ttf-dejavu noto-fonts-emoji papirus-icon-theme breeze-icons plymouth pavucontrol-qt nitrogen \
+kitty python-pillowfight xterm thunar thunar-archive-plugin thunar-volman tumbler ffmpegthumbnailer \
+engrampa scrot atril imv mpv parole mousepad galculator timeshift bash-completion vim nano micro fzf \
+lazygit htop fastfetch cmus cava ranger ueberzug highlight atool w3m mediainfo perl-image-exiftool \
+signal-desktop gpick darktable gimp inkscape filezilla libreoffice
 
 if ! command -v yay > /dev/null 2>&1; then
     echo "========================================================================"
