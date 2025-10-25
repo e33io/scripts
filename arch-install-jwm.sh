e56f3c54d1a1ac53ff2e0dc864df5a45ebc241de
e33io <info@e33.io>
2025-10-25 05:39:03 -0500

    Update install commands

--- arch-install-jwm.sh
+++ arch-install-jwm.sh
@@ -43,7 +43,7 @@ echo "========================================================================"
 sudo pacman -S --noconfirm --needed xorg-server xorg-apps polybar i3lock xss-lock rofi dunst gvfs nfs-utils \
 cifs-utils fuse rsync cronie git curl wget tar less 7zip base-devel xsel xclip playerctl dex mate-polkit upower \
 xdg-desktop-portal-gtk xdg-user-dirs wmctrl xbindkeys xdotool lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings \
-lxappearance-gtk3 gnome-themes-extra gtk-engine-murrine qt5ct qt6ct ttf-dejavu noto-fonts-emoji papirus-icon-theme \
+lxappearance gnome-themes-extra gtk-engine-murrine qt5ct qt6ct ttf-dejavu noto-fonts-emoji papirus-icon-theme \
 breeze-icons plymouth pavucontrol-qt nitrogen kitty python-pillowfight xterm thunar thunar-archive-plugin \
 thunar-volman tumbler ffmpegthumbnailer engrampa scrot atril imv mpv parole mousepad galculator timeshift \
 bash-completion vim nano micro fzf lazygit htop fastfetch cmus cava ranger ueberzug highlight atool w3m mediainfo \
