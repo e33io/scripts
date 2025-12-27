266015d4b9c732c09eec3fad7d264c6d4cf72740
e33io <info@e33.io>
2025-12-26 20:35:00 -0600

    Update formatting

--- install-helium-browser.sh
+++ install-helium-browser.sh
@@ -10,19 +10,19 @@ HELIUM_VERSION=$(curl -s "https://api.github.com/repos/imputnet/helium-linux/rel
 | grep -Po '"tag_name": "\K[^"]*')
 mkdir -p "$HOME"/.local/applications
 wget -qO "$HOME"/.local/applications/helium.AppImage \
-https://github.com/imputnet/helium-linux/releases/download/${HELIUM_VERSION}/helium-${HELIUM_VERSION}-x86_64.AppImage
+https://github.com/imputnet/helium-linux/releases/download/"${HELIUM_VERSION}"/helium-"${HELIUM_VERSION}"-x86_64.AppImage
 chmod a+x "$HOME"/.local/applications/helium.AppImage && echo "Helium Browser is now installed."
 
 # add launch script with GTK environment variable
 mkdir -p "$HOME"/.local/bin
-printf '%s\n' '#!/bin/bash' '' 'bash -c "GDK_SCALE=1 "$HOME"/.local/applications/helium.AppImage"' \
+printf '%s\n' '#!/bin/bash' '' 'bash -c "GDK_SCALE=1 $HOME/.local/applications/helium.AppImage"' \
 > "$HOME"/.local/bin/helium-browser.sh
 chmod +x "$HOME"/.local/bin/helium-browser.sh
 
 # add .desktop file
 mkdir -p "$HOME"/.local/share/applications
 printf '%s\n' '[Desktop Entry]' 'Version=1.0' 'Name=Helium Browser' 'GenericName=Web Browser' \
-'Comment=Access the Internet' 'StartupWMClass=helium-browser' 'Exec=bash -c "bash "$HOME"/.local/bin/helium-browser.sh"' \
+'Comment=Access the Internet' 'StartupWMClass=helium-browser' 'Exec=bash -c "bash $HOME/.local/bin/helium-browser.sh"' \
 'StartupNotify=true' 'Terminal=false' 'Icon=internet-web-browser' 'Type=Application' 'Categories=Network;WebBrowser;' \
 'MimeType=application/pdf;application/rdf+xml;application/rss+xml;application/xhtml+xml;application/xhtml_xml;application/xml;image/gif;image/jpeg;image/png;image/webp;text/html;text/xml;x-scheme-handler/http;x-scheme-handler/https;x-scheme-handler/ipfs;x-scheme-handler/ipns;' \
 > "$HOME"/.local/share/applications/helium-browser.desktop
