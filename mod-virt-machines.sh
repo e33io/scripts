037534c044063f23fb3d8a25ad3f77ea9cf3ea37
e33io <info@e33.io>
2026-03-29 09:41:52 -0500

    Update commands and formatting

--- mod-virt-machines.sh
+++ mod-virt-machines.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # =============================================================================
-# Install spice-vdagent and update VM-specific configs
+# Install spice-vdagent and update window manager configs
 # URL: https://github.com/e33io/scripts/blob/main/mod-virt-machines.sh
 # -----------------------------------------------------------------------------
 # Use this script at your own risk, it will overwrite existing files!
@@ -19,22 +19,22 @@ fi
 # install spice-vdagent
 sudo pacman -S --noconfirm --needed spice-vdagent
 
-# i3wm specific configs
-if [ -d ~/.config/i3 ]; then
-    mkdir -p ~/.config/autostart
-    # add audio-default.desktop file
-    printf "%s\n" "[Desktop Entry]" "Type=Application" "Name=audio-default" "Comment=set default volume level" \
-    "Exec=sh -c 'sleep 2; pactl set-sink-volume @DEFAULT_SINK@ 75%'" "Icon=xfce4-mixer" "StartupNotify=false" \
-    "Terminal=false" "NoDisplay=true" > ~/.config/autostart/audio-default.desktop
-    # add xrandr-vm.desktop file
-    printf "%s\n" "[Desktop Entry]" "Version=1.0" "Type=Application" "Name=xrandr-vm" \
-    "Comment=Set xrandr resolution" "Exec=sh -c 'xrandr -s 3840x2160'" "StartupNotify=false" \
-    "Terminal=false" "NoDisplay=true" > ~/.config/autostart/xrandr-vm.desktop
-    # update xrandr monitor resolution if needed
-    Xft_dpi=$(grep -E '^Xft\.dpi' ~/.Xresources 2>/dev/null | grep -Eo '[0-9]+')
-    if [ "$Xft_dpi" = "96" ]; then
-        sed -i 's/3840x2160/1920x1080/' ~/.config/autostart/xrandr-vm.desktop
-    fi
-    # update lightdm Xgsession file
-    sudo sed -i 's/GDK_SCALE=2/GDK_SCALE=1/' /etc/lightdm/Xgsession
+mkdir -p ~/.config/autostart
+
+# add audio-default.desktop file
+printf "%s\n" "[Desktop Entry]" "Type=Application" "Name=audio-default" "Comment=set default volume level" \
+"Exec=sh -c 'sleep 2; pactl set-sink-volume @DEFAULT_SINK@ 75%'" "Icon=xfce4-mixer" "StartupNotify=false" \
+"Terminal=false" "NoDisplay=true" > ~/.config/autostart/audio-default.desktop
+
+# add xrandr-vm.desktop file
+printf "%s\n" "[Desktop Entry]" "Version=1.0" "Type=Application" "Name=xrandr-vm" \
+"Comment=Set xrandr resolution" "Exec=sh -c 'xrandr -s 3840x2160'" "StartupNotify=false" \
+"Terminal=false" "NoDisplay=true" > ~/.config/autostart/xrandr-vm.desktop
+# update xrandr monitor resolution if needed
+Xft_dpi=$(grep -E '^Xft\.dpi' ~/.Xresources 2>/dev/null | grep -Eo '[0-9]+')
+if [ "$Xft_dpi" = "96" ]; then
+    sed -i 's/3840x2160/1920x1080/' ~/.config/autostart/xrandr-vm.desktop
 fi
+
+# update lightdm Xgsession file
+sudo sed -i 's/GDK_SCALE=2/GDK_SCALE=1/' /etc/lightdm/Xgsession
